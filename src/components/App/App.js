d4adb24f33d24ac80c0bc10c063f00a4e52dd32d
donnyesq <adonis@darmas.ca>
2020-09-02 21:18:57 -0400

    exercise 5 complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import GlobalStyle from "./GlobalStyle";
+
+const DEFAULT_ARTIST_ID = "2RhgnQNC74QoBlaUvT4MEe";
+const defaultArtistPath = `/artists/${DEFAULT_ARTIST_ID}`;
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Router>Hello</Router>
+      <Router>
+        <Switch>
+          <Route exact path="/artists/:id">
+            <div>Artists Page</div>
+          </Route>
+          <Route exact path="/">
+            <Redirect to={{ pathname: defaultArtistPath }} />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
